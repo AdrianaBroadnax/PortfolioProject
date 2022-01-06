c11644bccaa867bcf60c8d3abfcdad000db17333
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-01-05 21:01:55 -0500

    added html and js folder

---
+++ JS/about.js
@@ -0,0 +1,7 @@
+var aboutMeBtn = document.querySelector("#aboutMeButton");
+
+const aboutMeContainer = document.getElementById("aboutMeContainer");
+startBtn.addEventListener("click", function () {
+  console.log("start");
+  document.location = "about.html";
+});
---
+++ JS/contacts.js
@@ -0,0 +1,8 @@
+var contactBtn = document.querySelector("#contactButton");
+
+
+const contactButtonContainer = document.getElementById("contactButtonContainer");
+startBtn.addEventListener("click", function () {
+  console.log("start");
+  document.location = "contacts.html";
+});
